5c1bc9610a452fed0b591a641bff3d2af273cc70
DerNerger <fe.kibellus@gmail.com>
2014-10-24 14:31:44 +0200

    errors

--- Semeserkurs/Aufgabe3/3.4.cpp
+++ Semeserkurs/Aufgabe3/3.4.cpp
@@ -1,55 +1,71 @@
 #include <iostream>
+#include <cctype>
 using namespace std;
 
 int readExpr();
 int readSum();
 int readFak();
 void overreadBlanks();
+int error = 0;
 
 int main() {
 	cout << "Bitte Term eingeben:" << endl;
-	cout << readExpr() << endl;
-	return 0;
+	int result = readExpr();
+        if(error){
+            cout << "Synaxfehler!" << endl;
+            return 1;
+        } else {
+            cout << result << endl;
+	    return 0;
+        }
 }
 
 int readExpr(){
 	int lhs = readSum();
-	if(true){
+	if(!error){
 		overreadBlanks();
 		while((cin.peek() == '+') || (cin.peek() == '-')){
 			int rhs = readSum();
-			if(true){
+			if(!error){
 				lhs += rhs;
 				overreadBlanks();
-			}
+			} else {
+                            return 0;
+                        }
 		}
-	}
+	} else {
+            return 0;
+        }
 	return lhs;
 }
 
 void overreadBlanks(){
-	char c;
-	while(cin.peek()==' ')
-		cin.get(c);
+    char c;
+    if(isspace(cin.peek()))
+        cin.get(c);
 }
 
 int readSum(){
 	int lhs = readFak();
-	if(true){
+	if(!error){
 		overreadBlanks();
 		while((cin.peek() == '*') || (cin.peek() == '/')){
 			char c;
 			cin.get(c);
 			int rhs = readFak();
-			if(true){
+			if(!error){
 				if(c=='*')
 					lhs *= rhs;
 				else
 					lhs /= rhs;
 				overreadBlanks();
-			}
+			} else {
+                            return 0;
+                        }
 		}
-	}
+	} else {
+            return 0;
+        }
 	return lhs;
 }
 
@@ -63,18 +79,27 @@ int readFak(){
 		if(c=='-')
 			result *= -1;
 	} else {
-		if((cin.peek() >= '0') && (cin.peek()<='9')){ //isdidgit
+		if(isdigit(cin.peek())){ //isdigit
 			cin >> result;
 		} else {
 			//Klammer
 			cin.get(c);
 			if(c == '('){
 				result = readExpr();
-				if(true){
+				if(!error){
 					overreadBlanks();
 					cin.get(c); //read )
-				}
-			}
+                                        if(c!= ')'){
+                                            error = 1;
+                                            return 0;
+                                        }
+				} else {
+                                    return 0;
+                                }
+			} else {
+                            error = 1;
+                            return 0;
+                        }
 		}
 	}
 	return result;
